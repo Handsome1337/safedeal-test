4cde14b43a95993cb03aceb30c40fa3488826ef3
Ilya Volkov <volkov-ilyha@ya.ru>
2020-01-06 13:27:38 +0300

    Подготавливает вёрстку и стили

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const path = require(`path`);
 
 module.exports = {
   mode: `development`,
-  entry: `./source/main.js`,
+  entry: `./source/js/main.js`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
   devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, `public`),
+    contentBase: path.join(__dirname, `source`),
     publicPath: `http://localhost:8080/`,
     compress: true,
     watchContentBase: true
